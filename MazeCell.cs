2da3baa801abecc47b9df07e72b39e83625a3f21
TanasieNicusor <tanasie.cristian.g3c@student.ucv.ro>
2025-05-21 13:57:56 +0300

    Add files via upload

--- MazeCell.cs
+++ MazeCell.cs
@@ -1,21 +1,13 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MazeCell : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject _leftWall;
-
-    [SerializeField]
-    private GameObject _rightWall;
-
-    [SerializeField]
-    private GameObject _frontWall;
-
-    [SerializeField]
-    private GameObject _backWall;
-
-    [SerializeField]
-    private GameObject _unvisitedBlock;
+    [SerializeField] private GameObject _leftWall;
+    [SerializeField] private GameObject _rightWall;
+    [SerializeField] private GameObject _frontWall;
+    [SerializeField] private GameObject _backWall;
+    [SerializeField] private GameObject _unvisitedBlock;
 
     public bool IsVisited { get; private set; }
 
@@ -27,23 +19,36 @@ public class MazeCell : MonoBehaviour
 
     public void ClearLeftWall()
     {
-        _leftWall.SetActive(false);
+        DisableWall(_leftWall);
     }
 
     public void ClearRightWall()
     {
-        _rightWall.SetActive(false);
+        DisableWall(_rightWall);
     }
 
     public void ClearFrontWall()
     {
-        _frontWall.SetActive(false);
+        DisableWall(_frontWall);
     }
 
     public void ClearBackWall()
     {
-        _backWall.SetActive(false);
+        DisableWall(_backWall);
     }
 
+    private void DisableWall(GameObject wall)
+    {
+        if (wall == null) return;
 
+        // Disable the wall visually
+        wall.SetActive(false);
+
+        // Disable the NavMeshObstacle if it exists
+        NavMeshObstacle obstacle = wall.GetComponent<NavMeshObstacle>();
+        if (obstacle != null)
+        {
+            obstacle.enabled = false;
+        }
+    }
 }
